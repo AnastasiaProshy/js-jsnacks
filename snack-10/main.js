8339051abeaf492d3da6aacae2e740805714fb5b
Anastasia <anastasia.ukhlina@gmail.com>
2024-05-20 18:50:57 +0200

    add: snack-10

---
+++ snack-10/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const array = [];
+let userInput = 0;
+let a = 0;
+
+while (a == 0) {
+    userInput = parseInt(prompt('Inserisci un numero:'));
+    array[0] = userInput;
+    if (array[0] == array[1]) {
+        console.log('I valori sono uguali');
+        a = 1;
+    }
+    else { console.log('I valori non sono uguali'); }
+    array[1] = array[0]
+
+}
